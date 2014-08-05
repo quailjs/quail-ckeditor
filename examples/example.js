cfc1828349c1d79a5a489854d36efdafaf872172
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-08-04 22:32:42 -0400

    Update the examples with more tests and example content

--- examples/example.js
+++ examples/example.js
@@ -3,16 +3,36 @@
     {
       toolbar: [
         { name: 'basicstyles', items: [ 'Quail', '-','Bold','Italic' ] },
-  			{ name: 'paragraph', items : [ 'NumberedList','BulletedList' ] },
-  			{ name: 'tools', items : [ 'Image','Maximize' ] }
+  			{ name: 'paragraph', items : [ 'Format', 'NumberedList','BulletedList' ] },
+  			{ name: 'tools', items : [ 'Image','Maximize', 'Link', 'Source' ] },
+        '/',
       ],
       extraPlugins: 'quail,dialog',
       quail: {
         path: '../lib/quail',
         tests: [
           'imgHasAlt',
-          'pNotUsedAsHeader'
+          'pNotUsedAsHeader',
+          'headerH1',
+          'headerH1Format',
+          'headerH2',
+          'headerH2Format',
+          'headerH3',
+          'headerH3Format',
+          'headerH4',
+          'headerH4Format',
+          'headerH5Format',
+          'headerH6Format',
+          'aAdjacentWithSameResourceShouldBeCombined',
+          'aImgAltNotRepetitive',
+          'aLinkTextDoesNotBeginWithRedundantWord',
+          'aLinksAreSeparatedByPrintableCharacters',
+          'aLinksDontOpenNewWindow',
+          'aMustContainText'
         ]
-      }
+      },
+      // Set the most common block elements.
+      format_tags: 'p;h1;h2;h3;pre',
+      removeDialogTabs: ''
     });
 })(jQuery, CKEDITOR);
